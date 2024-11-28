51fa26d74279531c7a8efc565aedb32d4d5b9a1f
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-28 21:50:29 +0700

    Refactor logging code

--- main/tasks.py
+++ main/tasks.py
@@ -20,6 +20,7 @@ def send_email_task(
     subject: str, 
     content: str, 
 ) -> None:     
+    context_log_info = f'Отправитель: {sender}, получатель: {recipient}. Тема: {subject}'
     try:
         connection = get_connection(
             host=host,
@@ -37,19 +38,18 @@ def send_email_task(
             connection=connection
         )
     except SMTPAuthenticationError as e:
-        logger.error(f'Ошибка авторизации. Отправитель: {sender}, получатель: {recipient}. Тема: {subject}')
+        logger.error(f'Ошибка авторизации. ')
     except SMTPConnectError as e:
         if self.request.retries >= self.max_retries:
             logger.error(
-                f'Ошибка подключения: {e}. Отправитель: {sender}, получатель: {recipient}. Тема: {subject}'
+                f'Ошибка подключения: {e}. {context_log_info}'
             )
         self.retry(exc=e, countdown=20)  
     except SMTPException as e:
         self.retry(exc=e, countdown=20) 
     except Exception as e:
         if self.request.retries >= self.max_retries:
-            logger.error(f'Непредвиденная ошибка: {e}. Отправитель: {sender}, получатель: {recipient}. Тема: {subject}')
+            logger.error(f'Непредвиденная ошибка: {e}. {context_log_info}')
         raise self.retry(exc=e, countdown=20)
     
-    logger.info(f'Письмо было успешно отправлено. Отправитель: {sender}, получатель: {recipient}. Тема: {subject}')
-
+    logger.info(f'Письмо было успешно отправлено. {context_log_info}')
