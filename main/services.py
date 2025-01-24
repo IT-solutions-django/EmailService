29c342ef8fa366314fe637d3ac39728ba6fbee87
Пак Сергей <sergegnabri2005@gmail.com>
2025-01-24 18:45:15 +0700

    Try to fix

--- main/services.py
+++ main/services.py
@@ -3,11 +3,11 @@ from django.core.validators import validate_email
 
 
 def get_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = x_forwarded_for.split(',')[0]
     else:
-        ip = request.META.get('REMOTE_ADDR', '')
+        ip = request.META.get('REMOTE_ADDR')
 
     print(ip)
     return ip
